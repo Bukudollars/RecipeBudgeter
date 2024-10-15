64188f08a748bcd9a02bbf2dfab19ee7aced0511
james Wang <james.wang12345@gmail.com>
2024-10-15 14:46:45 -0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Typography, Box } from '@mui/material';
+
+export default function App() {
+    return (
+        <Box>
+            <Typography variant="h1" component="h1" align="center">
+                Recipe Budgeter
+            </Typography>
+        </Box>
+    );
+};
